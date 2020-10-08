f6905c7c447952e09de85046a7edba2cdb9b6b12
Sohaib Salman <sohaibsalman10@gmail.com>
2020-10-08 19:04:47 +0500

    Refactor mainComponentConsts.js file

--- intelligent_remote_academia-frontend/src/components/constants/mainContentConsts.js
+++ intelligent_remote_academia-frontend/src/components/constants/mainContentConsts.js
@@ -1,9 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   container: {
-    marginTop: 100,
+    marginTop: 80,
   },
-}));
+});
 
 export { useStyles };
