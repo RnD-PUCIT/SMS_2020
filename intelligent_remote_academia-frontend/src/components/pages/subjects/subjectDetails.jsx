33a553e54d46829ddc989ae479096008ac5cc1cb
Sohaib Salman <sohaibsalman10@gmail.com>
2020-10-18 14:59:12 +0500

    Create SubjectDetail component

---
+++ intelligent_remote_academia-frontend/src/components/pages/subjects/subjectDetails.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class SubjectDetails extends Component {
+  state = {};
+  render() {
+    return <h1>Subject Details</h1>;
+  }
+}
+
+export default SubjectDetails;
