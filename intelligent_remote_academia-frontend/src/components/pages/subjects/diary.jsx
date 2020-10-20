e59c98e4af856e90d2895831c7e92264d2a6d84a
Sohaib Salman <sohaibsalman10@gmail.com>
2020-10-20 14:41:21 +0500

    Add dropdown for months to filter diary content

--- intelligent_remote_academia-frontend/src/components/pages/subjects/diary.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/subjects/diary.jsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { Button, Paper, Toolbar, Typography } from "@material-ui/core";
+import React, { Component } from "react";
+import {
+  Button,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+  Typography,
+} from "@material-ui/core";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
@@ -14,6 +23,7 @@ const useStyles = makeStyles({
     borderTopLeftRadius: "4px",
     borderTopRightRadius: "4px",
     color: "white",
+    padding: "5px 20px",
   },
 });
 
@@ -25,6 +35,17 @@ const buttonStyles = makeStyles((theme) => ({
   },
 }));
 
+const menuStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexGrow: 1,
+  },
+  paper: {
+    position: "relative",
+    zIndex: 9999,
+  },
+});
+
 const Diary = ({ diary }) => {
   const classes = useStyles();
   return (
@@ -34,9 +55,9 @@ const Diary = ({ diary }) => {
       {diary.map((item) => {
         return (
           <Paper className={classes.paper}>
-            <Toolbar className={classes.toolbar} variant="dense">
+            <div className={classes.toolbar} variant="dense">
               <Typography variant="h6">{item.diary_date}</Typography>
-            </Toolbar>
+            </div>
             <div className={classes.paperBody}>
               <Typography>{item.diary_title}</Typography>
               {item.diary_content && (
@@ -51,7 +72,61 @@ const Diary = ({ diary }) => {
 };
 
 const DiaryFilterMenu = () => {
-  return <h1>Menu</h1>;
+  const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const classes = menuStyles();
+  return (
+    <React.Fragment>
+      <div className={classes.root}></div>
+      <Button ref={anchorRef} onClick={handleToggle} variant="outlined">
+        Select Month
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper className={classes.paper}>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList autoFocusItem={open} id="menu-list-grow">
+                  {months.map((month) => {
+                    return (
+                      <MenuItem onClick={handleClose} key={month.id}>
+                        {month.name}
+                      </MenuItem>
+                    );
+                  })}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </React.Fragment>
+  );
 };
 
 const DiaryFilterButtons = () => {
@@ -90,3 +165,54 @@ const DiaryContent = ({ content }) => {
 };
 
 export default Diary;
+
+const months = [
+  {
+    id: 1,
+    name: "January",
+  },
+  {
+    id: 2,
+    name: "February",
+  },
+  {
+    id: 3,
+    name: "March",
+  },
+  {
+    id: 4,
+    name: "April",
+  },
+  {
+    id: 5,
+    name: "May",
+  },
+  {
+    id: 6,
+    name: "June",
+  },
+  {
+    id: 7,
+    name: "July",
+  },
+  {
+    id: 8,
+    name: "August",
+  },
+  {
+    id: 9,
+    name: "September",
+  },
+  {
+    id: 10,
+    name: "October",
+  },
+  {
+    id: 11,
+    name: "November",
+  },
+  {
+    id: 12,
+    name: "December",
+  },
+];
