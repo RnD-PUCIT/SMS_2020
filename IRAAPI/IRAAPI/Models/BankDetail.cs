78abd4b1ca22967e1912fc4455cdeb2c103a79aa
arslanyousaf77 <arslanyousaf77@gmail.com>
2020-12-08 22:11:37 +0500

    EF Conversion SP1 completed with SessionId and SessionYear

---
+++ IRAAPI/IRAAPI/Models/BankDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace IRAAPI.Models
+{
+    [Table("Bank_Details")]
+    public partial class BankDetail
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("bank_name")]
+        public string BankName { get; set; }
+        [Required]
+        [Column("bank_branch")]
+        public string BankBranch { get; set; }
+        [Column("account_no")]
+        public long AccountNo { get; set; }
+        [Column("guid")]
+        public Guid Guid { get; set; }
+    }
+}
