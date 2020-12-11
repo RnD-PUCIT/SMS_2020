a32a82e90a1958e1ad1345ea7ce0f67454478dd7
Tehreem Akhter <tehreemakhtar44@gmail.com>
2020-12-11 19:04:43 +0500

    Receive Announcements data from API

--- intelligent_remote_academia-frontend/src/components/layouts/mainContent/mainContent.jsx
+++ intelligent_remote_academia-frontend/src/components/layouts/mainContent/mainContent.jsx
@@ -1,31 +1,40 @@
-import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
-import Subjects from "../../pages/subjects/subjects";
-import Attendance from "../../pages/attendance/attendance";
-import SubjectDetails from "../../pages/subjects/subjectDetails";
-import GradeDetails from "../../pages/subjects/gradeDetails";
-import Announcements from "../../pages/announcements/announcements";
+import Subjects from '../../pages/subjects/subjects';
+import Attendance from '../../pages/attendance/attendance';
+import SubjectDetails from '../../pages/subjects/subjectDetails';
+import GradeDetails from '../../pages/subjects/gradeDetails';
+import Announcements from '../../pages/announcements/announcements';
 
-import { useStyles } from "../../constants/mainContentConsts";
-import FeeChallan from "../../pages/fee-challan/feeChallan";
+import { useStyles } from '../../constants/mainContentConsts';
+import FeeChallan from '../../pages/fee-challan/feeChallan';
 
 const MainContent = ({ subjects, studentId, classId, sessionId }) => {
   const classes = useStyles();
   return (
-    <main className="container">
+    <main className='container'>
       <div className={classes.container}>
         <Switch>
           <Route
-            path="/subjects/:subjectSlug/:gradeTypeSlug"
+            path='/subjects/:subjectSlug/:gradeTypeSlug'
             component={GradeDetails}
           />
-          <Route path="/subjects/:subjectSlug" component={SubjectDetails} />
-          <Route path="/announcements" component={Announcements} />
+          <Route path='/subjects/:subjectSlug' component={SubjectDetails} />
+          <Route
+            path='/announcements'
+            render={() => (
+              <Announcements
+                studentId={studentId}
+                classId={classId}
+                sessionId={sessionId}
+              />
+            )}
+          />
 
           {/* Sending subjects array as a prop to Subject component */}
           <Route
-            path="/subjects"
+            path='/subjects'
             render={() => (
               <Subjects
                 subjects={subjects}
@@ -36,7 +45,7 @@ const MainContent = ({ subjects, studentId, classId, sessionId }) => {
             )}
           />
           <Route
-            path="/attendance"
+            path='/attendance'
             render={() => (
               <Attendance
                 studentId={studentId}
@@ -46,12 +55,12 @@ const MainContent = ({ subjects, studentId, classId, sessionId }) => {
             )}
           />
           <Route
-            path="/challan"
+            path='/challan'
             render={() => (
               <FeeChallan studentId={studentId} classId={classId} />
             )}
           />
-          <Redirect from="/" to="/subjects" exact />
+          <Redirect from='/' to='/subjects' exact />
         </Switch>
       </div>
     </main>
--- intelligent_remote_academia-frontend/src/components/pages/announcements/announcements.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/announcements/announcements.jsx
@@ -33,101 +33,159 @@ const useStyles = makeStyles({
   },
 });
 
-class AnnouncementData extends Component {
+class Announcements extends Component {
   state = {
-    selectedTab=0,
-    filter='',
+    selectedTab: 0,
+    filter: '',
     instituteAnnouncements: null,
     classAnnouncements: null,
     studentAnnouncements: null,
   };
 
   async componentDidMount() {
-    const { studentId, classId } = this.props;
+    const { studentId, classId, sessionId } = this.props;
 
-    const url = '/announcements?studentid=' + studentId + '&classid=' + classId;
+    const url =
+      '/announcements?studentid=' +
+      studentId +
+      '&classid=' +
+      classId +
+      '&sessionid=' +
+      sessionId;
     const { data } = await http.get(`${url}`);
+    console.log(data);
 
     const { ins, classs, stdnt } = data;
+    console.log(ins);
+
     this.setState({
       instituteAnnouncements: ins,
       classAnnouncements: classs,
       studentAnnouncements: stdnt,
     });
   }
-  // render() {
-  //   return (  );
-  // }
+
+  render() {
+    //event for handling changing tabs
+    const handleChange = (event, newValue) => {
+      this.setState({ selectedTab: newValue });
+    };
+
+    // //event for handling searchResults
+    // const handleSearchChange = (e, value) => {
+    //   setFilter(e.target.value);
+    //   value && setFilter(value);
+    // };
+
+    return (
+      <React.Fragment>
+        <Tabs
+          value={this.state.selectedTab}
+          onChange={handleChange}
+          centered
+          style={{ margin: '30px 0 40px 0' }}
+          indicatorColor='primary'
+          textColor='primary'>
+          {announcementTabs.map((tab) => {
+            return <Tab key={tab.id} label={tab.name} />;
+          })}
+        </Tabs>
+        {/* <SearchInput
+          data={data}
+          label={'Search announcements by subject'}
+          onSearchChange={handleSearchChange}
+        /> */}
+        {/* {this.state.selectedTab === 0 && (
+          <AnnouncementContent
+            announcements={this.state.instituteAnnouncements}
+            // filter={filter}
+          />
+        )}
+        {this.state.selectedTab === 1 && (
+          <AnnouncementContent
+            announcements={this.state.classAnnouncements}
+            // filter={filter}
+          />
+        )}
+        {this.state.selectedTab === 2 && (
+          <AnnouncementContent
+            announcements={this.state.studentAnnouncements}
+            // filter={filter}
+          />
+        )} */}
+      </React.Fragment>
+    );
+  }
 }
 
-const Announcements = () => {
-  const [selectedTab, setSelectedTab] = React.useState(0);
-  const [filter, setFilter] = React.useState('');
+// const Announcements = () => {
+//   const [selectedTab, setSelectedTab] = React.useState(0);
+//   const [filter, setFilter] = React.useState('');
 
-  //event for handling changing tabs
-  const handleChange = (event, newValue) => {
-    setSelectedTab(newValue);
-  };
+//   //event for handling changing tabs
+//   const handleChange = (event, newValue) => {
+//     setSelectedTab(newValue);
+//   };
 
-  //event for handling searchResults
-  const handleSearchChange = (e, value) => {
-    setFilter(e.target.value);
-    value && setFilter(value);
-  };
+//   //event for handling searchResults
+//   const handleSearchChange = (e, value) => {
+//     setFilter(e.target.value);
+//     value && setFilter(value);
+//   };
 
-  //title of all announcement objects
-  const {
-    instituteAnnouncements,
-    classAnnouncements,
-    studentAnnouncements,
-  } = Announcement;
+//   //title of all announcement objects
+//   const {
+//     instituteAnnouncements,
+//     classAnnouncements,
+//     studentAnnouncements,
+//   } = Announcement;
 
-  let data = [
-    ...instituteAnnouncements,
-    ...classAnnouncements,
-    ...studentAnnouncements,
-  ];
-  data = data.map((item) => item.title);
+//   let data = [
+//     ...instituteAnnouncements,
+//     ...classAnnouncements,
+//     ...studentAnnouncements,
+//   ];
+//   data = data.map((item) => item.title);
 
-  return (
-    <React.Fragment>
-      <Tabs
-        value={selectedTab}
-        onChange={handleChange}
-        centered
-        style={{ margin: '30px 0 40px 0' }}
-        indicatorColor='primary'
-        textColor='primary'>
-        {announcementTabs.map((tab) => {
-          return <Tab key={tab.id} label={tab.name} />;
-        })}
-      </Tabs>
-      <SearchInput
-        data={data}
-        label={'Search announcements by subject'}
-        onSearchChange={handleSearchChange}
-      />
-      {selectedTab === 0 && (
-        <AnnouncementContent
-          announcements={Announcement.instituteAnnouncements}
-          filter={filter}
-        />
-      )}
-      {selectedTab === 1 && (
-        <AnnouncementContent
-          announcements={Announcement.classAnnouncements}
-          filter={filter}
-        />
-      )}
-      {selectedTab === 2 && (
-        <AnnouncementContent
-          announcements={Announcement.studentAnnouncements}
-          filter={filter}
-        />
-      )}
-    </React.Fragment>
-  );
-};
+//   return (
+//     <React.Fragment>
+//       <Tabs
+//         value={selectedTab}
+//         onChange={handleChange}
+//         centered
+//         style={{ margin: '30px 0 40px 0' }}
+//         indicatorColor='primary'
+//         textColor='primary'>
+//         {announcementTabs.map((tab) => {
+//           return <Tab key={tab.id} label={tab.name} />;
+//         })}
+//       </Tabs>
+//       <SearchInput
+//         data={data}
+//         label={'Search announcements by subject'}
+//         onSearchChange={handleSearchChange}
+//       />
+//       {selectedTab === 0 && (
+//         <AnnouncementContent
+//           announcements={Announcement.instituteAnnouncements}
+//           filter={filter}
+//         />
+//       )}
+//       {selectedTab === 1 && (
+//         <AnnouncementContent
+//           announcements={Announcement.classAnnouncements}
+//           filter={filter}
+//         />
+//       )}
+//       {selectedTab === 2 && (
+//         <AnnouncementContent
+//           announcements={Announcement.studentAnnouncements}
+//           filter={filter}
+//         />
+//       )}
+//     </React.Fragment>
+//   );
+// };
 
 const AnnouncementContent = ({ announcements, filter }) => {
   const filteredAnnouncements = announcements.filter((item) =>
@@ -174,85 +232,85 @@ const AnnouncementContent = ({ announcements, filter }) => {
   );
 };
 
-const Announcement = {
-  instituteAnnouncements: [
-    {
-      title: 'Summer Holidays',
-      description: 'Summer Holidays will start from tomorrow',
-      date: '10/1/2020',
-    },
-    {
-      title: 'Timing Change',
-      description: 'Timings of Classes from 1 to 10 have been changed',
-      date: '10/2/2020',
-    },
-    {
-      title: 'Exams Schedule',
-      description: 'Exams will be scheduled from Monday at 8am',
-      date: '10/3/2020',
-    },
-    {
-      title: 'Winter Holidays',
-      description: 'Winter Holidays will start from tomorrow',
-      date: '10/4/2020',
-    },
-    {
-      title: 'Test Session',
-      description: 'Test session of all classes will start from next week',
-      date: '10/5/2020',
-    },
-  ],
+// const Announcement = {
+//   instituteAnnouncements: [
+//     {
+//       title: 'Summer Holidays',
+//       description: 'Summer Holidays will start from tomorrow',
+//       date: '10/1/2020',
+//     },
+//     {
+//       title: 'Timing Change',
+//       description: 'Timings of Classes from 1 to 10 have been changed',
+//       date: '10/2/2020',
+//     },
+//     {
+//       title: 'Exams Schedule',
+//       description: 'Exams will be scheduled from Monday at 8am',
+//       date: '10/3/2020',
+//     },
+//     {
+//       title: 'Winter Holidays',
+//       description: 'Winter Holidays will start from tomorrow',
+//       date: '10/4/2020',
+//     },
+//     {
+//       title: 'Test Session',
+//       description: 'Test session of all classes will start from next week',
+//       date: '10/5/2020',
+//     },
+//   ],
 
-  classAnnouncements: [
-    {
-      title: 'Makeup Classes',
-      description: 'Makeup Class of Physics will be held on Tuesday',
-      date: '10/1/2020',
-    },
-    {
-      title: 'Maths Class Timing Change',
-      description: 'Timings of Math Class has been changed from 9am to 10am',
-      date: '10/2/2020',
-    },
-    {
-      title: 'MidTerm Examinations',
-      description: 'MidTerm Exams will be scheduled from Monday at 8am',
-      date: '10/3/2020',
-    },
-    {
-      title: 'No Class of Chemistry',
-      description: 'There will be no class of chemistry tomorrow',
-      date: '10/5/2020',
-    },
-  ],
-  studentAnnouncements: [
-    {
-      title: 'Fee Submission',
-      description: 'Kindly submit your fees before due date',
-      date: '10/1/2020',
-    },
-    {
-      title: 'Attendance',
-      description: 'You have been absent from school for 2 days',
-      date: '10/2/2020',
-    },
-    {
-      title: 'Application Submission',
-      description: 'Your application for sick leave has been submitted',
-      date: '10/3/2020',
-    },
-    {
-      title: 'Late Comer',
-      description:
-        'Kindly be punctual otherwise you will not be permitted to enter in the class',
-      date: '10/4/2020',
-    },
-    {
-      title: 'Warning for bad Behaviour in class',
-      description: 'This your first warning for behaving bad in the class ',
-      date: '10/5/2020',
-    },
-  ],
-};
+//   classAnnouncements: [
+//     {
+//       title: 'Makeup Classes',
+//       description: 'Makeup Class of Physics will be held on Tuesday',
+//       date: '10/1/2020',
+//     },
+//     {
+//       title: 'Maths Class Timing Change',
+//       description: 'Timings of Math Class has been changed from 9am to 10am',
+//       date: '10/2/2020',
+//     },
+//     {
+//       title: 'MidTerm Examinations',
+//       description: 'MidTerm Exams will be scheduled from Monday at 8am',
+//       date: '10/3/2020',
+//     },
+//     {
+//       title: 'No Class of Chemistry',
+//       description: 'There will be no class of chemistry tomorrow',
+//       date: '10/5/2020',
+//     },
+//   ],
+//   studentAnnouncements: [
+//     {
+//       title: 'Fee Submission',
+//       description: 'Kindly submit your fees before due date',
+//       date: '10/1/2020',
+//     },
+//     {
+//       title: 'Attendance',
+//       description: 'You have been absent from school for 2 days',
+//       date: '10/2/2020',
+//     },
+//     {
+//       title: 'Application Submission',
+//       description: 'Your application for sick leave has been submitted',
+//       date: '10/3/2020',
+//     },
+//     {
+//       title: 'Late Comer',
+//       description:
+//         'Kindly be punctual otherwise you will not be permitted to enter in the class',
+//       date: '10/4/2020',
+//     },
+//     {
+//       title: 'Warning for bad Behaviour in class',
+//       description: 'This your first warning for behaving bad in the class ',
+//       date: '10/5/2020',
+//     },
+//   ],
+// };
 
 export default Announcements;
