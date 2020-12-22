a330e7c84fcd4d7ec799ca2ce230d9bb3dfbf8f1
arslanyousaf77 <arslanyousaf77@gmail.com>
2020-12-23 01:06:04 +0500

    TimetableController Added

---
+++ IRAAPI/IRAAPI/Migrations/20201222103709_days+timetable-models-added.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IRAAPI.Migrations
+{
+    public partial class daystimetablemodelsadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Days",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    day = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Days", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeTables",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    class_id = table.Column<int>(type: "int", nullable: false),
+                    session_id = table.Column<int>(type: "int", nullable: false),
+                    subject_id = table.Column<int>(type: "int", nullable: false),
+                    day_id = table.Column<int>(type: "int", nullable: false),
+                    time_slot = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeTables", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_TimeTables_Classes_class_id",
+                        column: x => x.class_id,
+                        principalTable: "Classes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TimeTables_Days_DaysId",
+                        column: x => x.day_id,
+                        principalTable: "Days",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TimeTables_Sessions_session_id",
+                        column: x => x.session_id,
+                        principalTable: "Sessions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TimeTables_Subjects_subject_id",
+                        column: x => x.subject_id,
+                        principalTable: "Subjects",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeTables_class_id",
+                table: "TimeTables",
+                column: "class_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeTables_DaysId",
+                table: "TimeTables",
+                column: "DaysId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeTables_session_id",
+                table: "TimeTables",
+                column: "session_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeTables_subject_id",
+                table: "TimeTables",
+                column: "subject_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeTables");
+
+            migrationBuilder.DropTable(
+                name: "Days");
+
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "Announcements");
+        }
+    }
+}
---
+++ IRAAPI/IRAAPI/Models/Days.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IRAAPI.Models
+{
+    public class Days
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("day")]
+        public string Day { get; set; }
+    }
+}
