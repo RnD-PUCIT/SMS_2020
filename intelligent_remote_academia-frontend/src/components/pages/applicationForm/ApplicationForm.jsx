b787838dc5044f2f4eb66cb260dee9fcb489ace7
Sohaib Salman <sohaibsalman10@gmail.com>
2020-12-30 16:58:28 +0500

    Add form in application form

--- intelligent_remote_academia-frontend/src/components/pages/applicationForm/ApplicationForm.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/applicationForm/ApplicationForm.jsx
@@ -1,14 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
+import {
+  Button,
+  Divider,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
-class ApplicationForm extends Component {
-  state = {};
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Application Form</h1>
-      </React.Fragment>
-    );
-  }
-}
+import useStyles from "../../../styles/applicationFornStyle";
+
+const ApplicationForm = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Paper variant="outlined" className={classes.root}>
+        <Typography variant="h5" align="center">
+          Application Form
+        </Typography>
+
+        <Divider style={{ margin: "15px 0" }} />
+
+        <Grid container spacing={1} style={{ marginTop: "15px" }}>
+          <Grid item md={8}>
+            <form>
+              <TextField
+                variant="outlined"
+                label="Subject"
+                placeholder="Enter your subject here"
+                fullWidth
+              />
+              <TextField
+                variant="outlined"
+                label="Application"
+                placeholder="Enter your application here"
+                rows={20}
+                multiline
+                fullWidth
+                style={{ marginTop: "10px" }}
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                color="primary"
+                style={{ marginTop: "10px" }}
+              >
+                Submit Application
+              </Button>
+            </form>
+          </Grid>
+          <Grid item md={4}>
+            <Typography variant="h6" align="center">
+              Upload additional files here
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </React.Fragment>
+  );
+};
 
 export default ApplicationForm;
---
+++ intelligent_remote_academia-frontend/src/styles/applicationFornStyle.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "30px",
+  },
+});
+
+export default useStyles;
