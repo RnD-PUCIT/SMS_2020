8ea5ef26ab94cba989f450968a8999598ab07094
Sohaib Salman <sohaibsalman10@gmail.com>
2021-01-01 14:22:19 +0500

    Add click event to submit button in application form

--- intelligent_remote_academia-frontend/src/components/pages/applicationForm/ApplicationForm.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/applicationForm/ApplicationForm.jsx
@@ -36,14 +36,21 @@ const formats = [
 const ApplicationForm = () => {
   const classes = useStyles();
 
-  const [subject, setSubject] = useState('');
+  const [subjectLine, setSubjectLine] = useState('');
+  const [applicationBody, setApplicationBody] = useState('');
 
-  const handleSubjectChange = (e) => {
-    console.log(e);
+  const handleApplicationContentChange = (content) => {
+    setApplicationBody(content);
   };
 
-  const handleBodyChange = (e) => {
-    // console.log(e.target.vale);
+  const handleSubmission = () => {
+    if (subjectLine === '' || subjectLine.length === 0) {
+      alert('Enter Subject Line');
+    } else {
+      const formData = new FormData();
+      formData.append('subjectLine', subjectLine);
+      formData.append('applicationBody', applicationBody);
+    }
   };
 
   return (
@@ -57,34 +64,34 @@ const ApplicationForm = () => {
 
         <Grid container spacing={3} style={{ marginTop: '15px' }}>
           <Grid item md={8}>
-            <form>
-              <TextField
-                variant="outlined"
-                label="Subject"
-                placeholder="Enter your subject here"
-                fullWidth
-                required
-                onChange={handleSubjectChange}
+            <TextField
+              variant="outlined"
+              label="Subject Line"
+              placeholder="Enter your subject here"
+              fullWidth
+              required
+              onChange={(event) => {
+                setSubjectLine(event.target.value);
+              }}
+            />
+            <div style={{ marginTop: '10px' }}>
+              <ReactQuill
+                className="application-editor"
+                modules={modules}
+                formats={formats}
+                onChange={handleApplicationContentChange}
               />
-              <div style={{ marginTop: '10px' }}>
-                <ReactQuill
-                  className="application-editor"
-                  modules={modules}
-                  formats={formats}
-                  onChange={handleBodyChange}
-                />
-              </div>
+            </div>
 
-              <Button
-                variant="contained"
-                fullWidth
-                color="primary"
-                style={{ marginTop: '10px' }}
-                type="submit"
-              >
-                Submit Application
-              </Button>
-            </form>
+            <Button
+              variant="contained"
+              fullWidth
+              color="primary"
+              style={{ marginTop: '10px' }}
+              onClick={handleSubmission}
+            >
+              Submit Application
+            </Button>
           </Grid>
           <Grid item md={4}>
             <Typography variant="h6" align="center">
