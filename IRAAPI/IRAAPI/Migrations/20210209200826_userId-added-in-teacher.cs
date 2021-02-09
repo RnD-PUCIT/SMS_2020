6714a535f66a7a6586ce13c27fd145603852984c
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-02-10 01:09:11 +0500

    migration userId added

---
+++ IRAAPI/IRAAPI/Migrations/20210209200826_userId-added-in-teacher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IRAAPI.Migrations
+{
+    public partial class userIdaddedinteacher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Teachers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "user_id",
+                table: "Teachers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_UserId1",
+                table: "Teachers",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_AspNetUsers_UserId1",
+                table: "Teachers",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_AspNetUsers_UserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teachers_UserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "Teachers");
+        }
+    }
+}
