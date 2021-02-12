dc43f6e331284b560a2db384af4ed79728486a1e
Sohaib Salman <sohaibsalman10@gmail.com>
2021-02-12 12:51:19 +0500

    Change route on tab change

--- intelligent_remote_academia-frontend/src/components/pages/teacher/subjects/SubjectDashboard.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/teacher/subjects/SubjectDashboard.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import TabColored from '../../../common/tabs/TabColored';
+import Gradebook from '../gradebook/Gradebook';
 
-const SubjectDashboard = () => {
+const SubjectDashboard = (props) => {
   // State Variables
   const [tabLinks, setTabLinks] = useState([]);
+
+  // Prop variables
+  const { match } = props;
+  const { params } = match;
 
   // Custom variables
   const history = useHistory();
@@ -15,9 +20,11 @@ const SubjectDashboard = () => {
   }, []);
 
   // handler functions
-
   const handleTabClick = (tab) => {
-    console.log(tab);
+    const { classSlug } = params;
+    const { subjectSlug } = params;
+
+    history.push(`/classes/${classSlug}/${subjectSlug}${tab.url}`);
   };
 
   return (
@@ -30,6 +37,6 @@ const SubjectDashboard = () => {
 export default SubjectDashboard;
 
 const tabsConst = [
-  { text: 'Grade Book', url: '/gradebook' },
+  { text: 'Grade Book', url: '/gradebook', component: <Gradebook /> },
   { text: 'Diary', url: '/diary' },
 ];
