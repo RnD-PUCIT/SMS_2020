59b1586659b7835ccb199e50dd919fa89baba640
Sohaib Salman <sohaibsalman10@gmail.com>
2021-02-13 23:56:03 +0500

    Sort students by roll numbers in MarkGrade.jsx

--- intelligent_remote_academia-frontend/src/components/pages/teacher/gradebook/MarkGrade.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/teacher/gradebook/MarkGrade.jsx
@@ -20,6 +20,17 @@ const MarkGrade = () => {
   useEffect(() => {
     const grades = gradesConst;
 
+    grades.sort((a, b) => {
+      var x = a.studentInfo.rollNumber.toLowerCase();
+      var y = b.studentInfo.rollNumber.toLowerCase();
+      if (x < y) {
+        return -1;
+      }
+      if (x > y) {
+        return 1;
+      }
+    });
+
     // Set state variable
     setGradesList(grades);
   }, []);
@@ -31,7 +42,7 @@ const MarkGrade = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Student Information</TableCell>
+              <TableCell> Student Information</TableCell>
               <TableCell>Marks obtained</TableCell>
               <TableCell>Remarks</TableCell>
             </TableRow>
@@ -50,9 +61,9 @@ const MarkGrade = () => {
                   </ListItem>
                 </TableCell>
                 <TableCell>
-                  <TextField size="small" style={{width: '40px'}} />
-                  <span style={{fontSize: '25px'}}> / </span> 
-                  <span style={{fontSize: '16px'}}>{row.totalMarks}</span>
+                  <TextField size="small" style={{ width: '40px' }} />
+                  <span style={{ fontSize: '25px' }}> / </span>
+                  <span style={{ fontSize: '16px' }}>{row.totalMarks}</span>
                 </TableCell>
                 <TableCell>
                   <TextField size="small" fullWidth multiline />
@@ -78,6 +89,82 @@ const gradesConst = [
       rollNumber: 'BITF17A040',
       firstName: 'Sohaib',
       lastName: 'Salman',
+      profilePic: '',
+    },
+    gradeDate: '2/13/2021',
+    totalMarks: 100,
+    obtainedMarks: '',
+    gradeTitle: 'Quiz 01',
+    remarks: '',
+    classId: 1,
+    subjectId: 1,
+    gradeTypeId: 1,
+    sessionId: 1,
+  },
+  {
+    id: 1,
+    studentInfo: {
+      id: 1,
+      rollNumber: 'BITF17A012',
+      firstName: 'Arslan',
+      lastName: 'Yousaf',
+      profilePic: '',
+    },
+    gradeDate: '2/13/2021',
+    totalMarks: 100,
+    obtainedMarks: '',
+    gradeTitle: 'Quiz 01',
+    remarks: '',
+    classId: 1,
+    subjectId: 1,
+    gradeTypeId: 1,
+    sessionId: 1,
+  },
+  {
+    id: 1,
+    studentInfo: {
+      id: 1,
+      rollNumber: 'BITF17A026',
+      firstName: 'Daniyal',
+      lastName: 'Ahmed',
+      profilePic: '',
+    },
+    gradeDate: '2/13/2021',
+    totalMarks: 100,
+    obtainedMarks: '',
+    gradeTitle: 'Quiz 01',
+    remarks: '',
+    classId: 1,
+    subjectId: 1,
+    gradeTypeId: 1,
+    sessionId: 1,
+  },
+  {
+    id: 1,
+    studentInfo: {
+      id: 1,
+      rollNumber: 'BITF17A041',
+      firstName: 'Tehreem',
+      lastName: 'Akhter',
+      profilePic: '',
+    },
+    gradeDate: '2/13/2021',
+    totalMarks: 100,
+    obtainedMarks: '',
+    gradeTitle: 'Quiz 01',
+    remarks: '',
+    classId: 1,
+    subjectId: 1,
+    gradeTypeId: 1,
+    sessionId: 1,
+  },
+  {
+    id: 1,
+    studentInfo: {
+      id: 1,
+      rollNumber: 'BITF17A019',
+      firstName: 'Zainab',
+      lastName: 'Zulfiqar',
       profilePic: '',
     },
     gradeDate: '2/13/2021',
