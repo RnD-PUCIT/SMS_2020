0f3c3fa9b2768e53ccc0e3d23a2c834001f0192e
Sohaib Salman <sohaibsalman10@gmail.com>
2021-02-13 17:48:08 +0500

    Add New button to grade type heading

--- intelligent_remote_academia-frontend/src/components/pages/teacher/gradebook/GradeTypeList.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/teacher/gradebook/GradeTypeList.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { Button, makeStyles, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  gradeHeadingRoot: {
+    borderBottom: '1px solid black',
+    clear: 'both',
+  },
+  gradeHeading: {
+    display: 'inline-block',
+    padding: '20px',
+    margin: '0',
+  },
+  floatRight: {
+    float: 'right',
+    marginTop: '20px',
+  },
+});
 
 const GradeTypeList = ({ gradeTypeList }) => {
   return (
-    <React.Fragment>
+    <Paper className="paper_padding--sm u_mt_small">
       {gradeTypeList.map((gradeType, index) => {
-        return <h1 key={index}>{gradeType}</h1>;
+        return <GradeTypeHeading gradeType={gradeType} key={index} />;
       })}
-    </React.Fragment>
+    </Paper>
   );
 };
 
 export default GradeTypeList;
+
+const GradeTypeHeading = ({ gradeType }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.gradeHeadingRoot}>
+      <h2 className={classes.gradeHeading}>{gradeType}</h2>
+      <span className={classes.floatRight}>
+        <Button variant="contained">New</Button>
+      </span>
+    </div>
+  );
+};
