60f33dbe77c4956c83356fe073b600a591430097
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-02-17 15:10:11 +0500

    Diary dto created

--- IRAAPI/IRAAPI/Controllers/DiaryController.cs
+++ IRAAPI/IRAAPI/Controllers/DiaryController.cs
@@ -20,26 +20,53 @@ namespace IRAAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateDiary([FromBody] Diary diary)
+        public async Task<IActionResult> CreateDiary([FromBody] DiaryDTO DiaryDTO)
         {
             try
             {
-                if (diary != null)
+                if (DiaryDTO != null)
                 {
-                    diary.DiaryDate  = DateTime.Now; ;
-                    await context.Diaries.AddAsync(diary);
-                    await context.SaveChangesAsync();
-                    return Ok(new Response { Status = "Success", Message = "Diary created successfully!"  });
+                    int classNumericId = context.Classes.Where(c => c.Guid == DiaryDTO.ClassId)
+                        .Select(c => c.Id)
+                        .SingleOrDefault();
+                    int subjectNumericId = context.Subjects.Where(c => c.Guid == DiaryDTO.SubjectId)
+                        .Select(c => c.Id)
+                        .SingleOrDefault();
+                    int sessionNumericId = context.Sessions.Where(s => s.Guid == DiaryDTO.SessionId)
+                        .Select(s => s.Id)
+                        .SingleOrDefault();
+                    if(sessionNumericId != 0 && classNumericId != 0 && subjectNumericId != 0)
+                    {
+                        DiaryDTO.DiaryDate = DateTime.Now;
+
+                        Diary diary = new Diary { ClassId = classNumericId, SubjectId = subjectNumericId, SessionId = sessionNumericId, DiaryTitle = DiaryDTO.DiaryTitle, DiaryContent = DiaryDTO.DiaryContent, DiaryDate = DiaryDTO.DiaryDate };
+                        await context.Diaries.AddAsync(diary);
+                        await context.SaveChangesAsync();
+                        return Ok(new Response { Status = "Success", Message = "Diary created successfully!" });
+                    }
+                    else
+                    {
+                        return BadRequest(new Response { Status = "Error", Message = "Invalid reference data!" });
+                    }
                 }
-                return BadRequest(new Response { Status = "Error", Message = "Content is null" });
+                return BadRequest(new Response { Status = "Error", Message = "Content is null!" });
             }
             catch (Exception ex)
             {
-
                 return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = ex.ToString() });
             }
             
             
+        }
+
+        public class DiaryDTO
+        {   
+            public Guid ClassId { get; set; }
+            public Guid SubjectId { get; set; }
+            public Guid SessionId { get; set; }
+            public DateTime DiaryDate { get; set; }
+            public string DiaryTitle { get; set; }
+            public string DiaryContent { get; set; }
         }
     }
 }
