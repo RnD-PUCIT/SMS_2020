9feb85029703685d487360fc0bf7012c63f3c181
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-02-17 16:50:36 +0500

    optimized

--- IRAAPI/IRAAPI/Controllers/DiaryController.cs
+++ IRAAPI/IRAAPI/Controllers/DiaryController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
