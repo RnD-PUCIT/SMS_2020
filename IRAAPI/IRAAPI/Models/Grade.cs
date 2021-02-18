259b0940c3b64b16ab5308587d6a087aa24ac248
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-02-18 16:14:02 +0500

    grade model updated

--- IRAAPI/IRAAPI/Models/Grade.cs
+++ IRAAPI/IRAAPI/Models/Grade.cs
@@ -13,45 +13,26 @@ namespace IRAAPI.Models
         [Key]
         [Column("id")]
         public int Id { get; set; }
+
         [Column("student_id")]
+        [ForeignKey("Id")]
         public int StudentId { get; set; }
-        [Column("class_id")]
-        public int ClassId { get; set; }
-        [Column("subject_id")]
-        public int SubjectId { get; set; }
-        [Column("grade_date", TypeName = "date")]
-        public DateTime GradeDate { get; set; }
-        [Column("total_marks")]
-        public int TotalMarks { get; set; }
+        public virtual Student Student { get; set; }
+
         [Column("obtained_marks")]
         public int ObtainedMarks { get; set; }
-        [Column("grade_type_id")]
-        public int GradeTypeId { get; set; }
+
+        [Column("grade_activity_id")]
+        [ForeignKey("Id")]
+        public int GradeActivityId { get; set; }
+        public virtual GradeActivity GradeActivity { get; set; }
+
         [Required]
-        [Column("grade_title")]
-        [StringLength(50)]
-        public string GradeTitle { get; set; }
         [Column("remarks")]
         public string Remarks { get; set; }
-        [Column("session_id")]
-        public int SessionId { get; set; }
         [Column("guid")]
         public Guid Guid { get; set; }
 
-        [ForeignKey(nameof(ClassId))]
-        [InverseProperty("Grades")]
-        public virtual Class Class { get; set; }
-        [ForeignKey(nameof(GradeTypeId))]
-        [InverseProperty("Grades")]
-        public virtual GradeType GradeType { get; set; }
-        [ForeignKey(nameof(SessionId))]
-        [InverseProperty("Grades")]
-        public virtual Session Session { get; set; }
-        [ForeignKey(nameof(StudentId))]
-        [InverseProperty("Grades")]
-        public virtual Student Student { get; set; }
-        [ForeignKey(nameof(SubjectId))]
-        [InverseProperty("Grades")]
-        public virtual Subject Subject { get; set; }
+
     }
 }
