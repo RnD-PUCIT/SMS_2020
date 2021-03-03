76ecf24ae952ee0261b177ce28000c1749e3db95
Sohaib Salman <sohaibsalman10@gmail.com>
2021-03-03 12:50:47 +0500

    Change login.jsx from class component to functional component

--- intelligent_remote_academia-frontend/src/components/pages/login/login.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/login/login.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
-import { CssBaseline, withStyles } from '@material-ui/core';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -18,12 +18,10 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import jwt_decode from 'jwt-decode';
 
-import axios from 'axios';
-
 import AlertSimple from '../../common/alerts/alertSimple';
 import http from '../../../services/httpService';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   outerContainer: {
     background: 'linear-gradient(180deg, #778ca3 0%, #00d2d3 100%)',
   },
@@ -49,7 +47,7 @@ const styles = (theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-});
+}));
 
 const formSchema = Yup.object().shape({
   username: Yup.string()
@@ -62,158 +60,141 @@ const formSchema = Yup.object().shape({
     .min(3, 'Must be atleast 6 characters'),
 });
 
-class Login extends Component {
-  state = {
-    showPassword: false,
-    error: null,
-  };
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
+  const classes = useStyles();
 
-  handleClickShowPassword = () => {
-    this.setState({ showPassword: !this.state.showPassword });
-  };
+  const handleLogin = async (values) => {
+    const url = '/account/login';
 
-  render() {
-    const handleLogin = async (values) => {
-      const url = '/account/login';
+    try {
+      // Send Ajax call to the server
+      const { data } = await http.post(url, values);
 
-      try {
-        // Send Ajax call to the server
-        const { data } = await http.post(url, values);
+      // Get token
+      const { token: jwt } = data;
 
-        // Get token
-        const { token: jwt } = data;
+      // store it to local storage
+      localStorage.setItem('token', jwt);
 
-        // store it to local storage
-        localStorage.setItem('token', jwt);
+      const decoded = jwt_decode(jwt);
+      const { role } = decoded;
 
-        const decoded = jwt_decode(jwt);
-        const { role } = decoded;
-
-        // Redirect the user to dashboard
-        window.location = '/';
-      } catch (ex) {
-        if (ex.response && ex.response.status === 400) {
-          const error = ex.response.data;
-          this.setState({ error });
-        } else if (ex.response && ex.response.status === 404) {
-          window.location = '/notFound';
-        }
+      // Redirect the user to dashboard
+      window.location = '/';
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const err = ex.response.data;
+        setError(err);
+      } else if (ex.response && ex.response.status === 404) {
+        window.location = '/notFound';
       }
-    };
+    }
+  };
 
-    const { classes } = this.props;
-    return (
-      <div className={classes.outerContainer}>
-        <Container className={classes.container} maxWidth="sm" component="main">
-          <CssBaseline />
-          <div className={classes.root}>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Formik
-              initialValues={{
-                username: '',
-                password: '',
-              }}
-              validationSchema={formSchema}
-            >
-              {({ handleSubmit, getFieldProps, errors, touched, values }) => {
-                return (
-                  <form className={classes.form} onSubmit={handleSubmit}>
-                    <TextField
-                      variant="outlined"
-                      margin="normal"
-                      error={errors.username && touched.username ? true : false}
-                      fullWidth
-                      // id='username'
-                      label="Username"
-                      name="username"
-                      autoComplete="true"
-                      autoFocus
-                      {...getFieldProps('username')}
-                      helperText={
-                        errors.username && touched.username
-                          ? errors.username
-                          : ''
-                      }
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <PermIdentityIcon />
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                    <TextField
-                      variant="outlined"
-                      margin="normal"
-                      error={errors.password && touched.password ? true : false}
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type={this.state.showPassword ? 'text' : 'password'}
-                      // id='password'
-                      {...getFieldProps('password')}
-                      helperText={
-                        errors.password && touched.password
-                          ? errors.password
-                          : ''
-                      }
-                      InputProps={{
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <LockIcon />
-                          </InputAdornment>
-                        ),
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton
-                              onClick={this.handleClickShowPassword}
-                              aria-label="toggle password visibility"
-                              edge="end"
-                            >
-                              {this.state.showPassword ? (
-                                <Visibility />
-                              ) : (
-                                <VisibilityOff />
-                              )}
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                    {this.state.error && (
-                      <AlertSimple
-                        severity="error"
-                        message={this.state.error}
-                      />
-                    )}
-                    <FormControlLabel
-                      control={<Checkbox value="remember" color="primary" />}
-                      label="Remember me"
-                    />
-                    <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      className={classes.submit}
-                      onClick={() => handleLogin(values)}
-                    >
-                      Sign In
-                    </Button>
-                  </form>
-                );
-              }}
-            </Formik>
-          </div>
-        </Container>
-      </div>
-    );
-  }
-}
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-export default withStyles(styles)(Login);
+  return (
+    <div className={classes.outerContainer}>
+      <Container className={classes.container} maxWidth="sm" component="main">
+        <CssBaseline />
+        <div className={classes.root}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Formik
+            initialValues={{
+              username: '',
+              password: '',
+            }}
+            validationSchema={formSchema}
+          >
+            {({ handleSubmit, getFieldProps, errors, touched, values }) => {
+              return (
+                <form className={classes.form} onSubmit={handleSubmit}>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    error={errors.username && touched.username ? true : false}
+                    fullWidth
+                    // id='username'
+                    label="Username"
+                    name="username"
+                    autoComplete="true"
+                    autoFocus
+                    {...getFieldProps('username')}
+                    helperText={
+                      errors.username && touched.username ? errors.username : ''
+                    }
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <PermIdentityIcon />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    error={errors.password && touched.password ? true : false}
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    // id='password'
+                    {...getFieldProps('password')}
+                    helperText={
+                      errors.password && touched.password ? errors.password : ''
+                    }
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <LockIcon />
+                        </InputAdornment>
+                      ),
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            onClick={handleClickShowPassword}
+                            aria-label="toggle password visibility"
+                            edge="end"
+                          >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  {error && <AlertSimple severity="error" message={error} />}
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={() => handleLogin(values)}
+                  >
+                    Sign In
+                  </Button>
+                </form>
+              );
+            }}
+          </Formik>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
