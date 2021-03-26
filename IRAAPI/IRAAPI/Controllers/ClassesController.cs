42925ab16fb6975c91931d2f242858b1378cc7a2
Sohaib Salman <sohaibsalman10@gmail.com>
2021-03-27 00:05:09 +0500

    Add ParentController, ClassController, SessionController

---
+++ IRAAPI/IRAAPI/Controllers/ClassesController.cs
@@ -0,0 +1,27 @@
+﻿using IRAAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IRAAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly IRAAPIContext _context;
+
+        public ClassesController(IRAAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getClassesList")]
+        public async Task<ActionResult<List<Class>>> GetClassesList()
+        {
+            return await _context.Classes.ToListAsync();
+        }
+    }
+}
---
+++ IRAAPI/IRAAPI/Controllers/SessionController.cs
@@ -0,0 +1,27 @@
+﻿using IRAAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IRAAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly IRAAPIContext _context;
+
+        public SessionController(IRAAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getClassesList")]
+        public async Task<ActionResult<List<Session>>> GetClassesList()
+        {
+            return await _context.Sessions.ToListAsync();
+        }
+    }
+}
