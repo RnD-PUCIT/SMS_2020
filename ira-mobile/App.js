871d79c69a306092daa5a5abcdaecbfa8212204d
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-24 00:10:44 +0500

    Add welcome screen

--- ira-mobile/App.js
+++ ira-mobile/App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import WelcomScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Intelligent Remote Academia</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WelcomScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
