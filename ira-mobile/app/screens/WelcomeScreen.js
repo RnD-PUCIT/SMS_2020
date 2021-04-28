c47f8fdb54866ae11613038f65827c31a3dffc4c
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-28 21:22:56 +0500

    Create Welcome Screen

--- ira-mobile/app/screens/WelcomeScreen.js
+++ ira-mobile/app/screens/WelcomeScreen.js
@@ -1,16 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-function StartScreen() {
+import AppButton from "../components/buttons/AppButton";
+import colors from "../config/colors";
+
+function WelcomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Intelligent Remote Academia</Text>
-    </View>
+    <ImageBackground
+      blurRadius={1}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        {/* <Image style={styles.logo} source={require("../assets/logo-red.png")} /> */}
+        <Text style={styles.tagLine}>Intelligent Remote Academia</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton title="Login" />
+        {/* <AppButton title="Register" color="secondary" /> */}
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagLine: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 80,
+    color: colors.white,
+  },
 });
 
-export default StartScreen;
+export default WelcomeScreen;
