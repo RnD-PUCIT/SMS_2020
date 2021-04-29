041f870ff2e1a2180d6eacf678d61838e969b9e8
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-29 12:41:05 +0500

    Add students list to modal

--- ira-mobile/App.js
+++ ira-mobile/App.js
@@ -3,10 +3,53 @@ import React from "react";
 import Screen from "./app/components/Screen";
 import StudentPicker from "./app/components/StudentPicker";
 
+const students = [
+  {
+    id: 1,
+    firstName: "Sohaib",
+    lastName: "Salman",
+    className: "BS IT",
+    section: "F17 Afternoon",
+    profilePic: "",
+  },
+  {
+    id: 2,
+    firstName: "Arslan",
+    lastName: "Yousaf",
+    className: "BS IT",
+    section: "F17 Afternoon",
+    profilePic: "",
+  },
+  {
+    id: 3,
+    firstName: "Daniyal",
+    lastName: "Ahmed",
+    className: "BS IT",
+    section: "F17 Afternoon",
+    profilePic: "",
+  },
+  {
+    id: 4,
+    firstName: "Tehreem",
+    lastName: "Akhter",
+    className: "BS IT",
+    section: "F17 Afternoon",
+    profilePic: "",
+  },
+  {
+    id: 5,
+    firstName: "Zainab",
+    lastName: "Zulfiqar",
+    className: "BS IT",
+    section: "F17 Afternoon",
+    profilePic: "",
+  },
+];
+
 export default function App() {
   return (
     <Screen>
-      <StudentPicker />
+      <StudentPicker items={students} selectedItem={students[0]} />
     </Screen>
   );
 }
--- ira-mobile/app/components/StudentPicker.js
+++ ira-mobile/app/components/StudentPicker.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import {
   Button,
+  FlatList,
   Modal,
   StyleSheet,
+  TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
@@ -12,14 +14,14 @@ import AppText from "../components/AppText";
 import colors from "../config/colors";
 import Screen from "./Screen";
 
-function StudentPicker({ imageSrc, items, selectedItem }) {
+function StudentPicker({ items, selectedItem }) {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <React.Fragment>
       <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
         <View style={styles.container}>
-          {!imageSrc && (
+          {!selectedItem.profilePic && (
             <View style={styles.iconContainer}>
               <MaterialCommunityIcons
                 name="account"
@@ -29,8 +31,10 @@ function StudentPicker({ imageSrc, items, selectedItem }) {
             </View>
           )}
           <View style={styles.textContainer}>
-            <AppText>Sohaib Salman</AppText>
-            <AppText style={styles.subText}>Class - Section</AppText>
+            <AppText>{`${selectedItem.firstName} ${selectedItem.lastName}`}</AppText>
+            <AppText
+              style={styles.subText}
+            >{`${selectedItem.className} - ${selectedItem.section}`}</AppText>
           </View>
           <View style={styles.dropDownIcon}>
             <MaterialCommunityIcons
@@ -44,11 +48,40 @@ function StudentPicker({ imageSrc, items, selectedItem }) {
       <Modal visible={modalVisible} animationType="slide">
         <Screen>
           <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <StudenItem item={item} />}
+          />
         </Screen>
       </Modal>
     </React.Fragment>
   );
 }
+
+const StudenItem = ({ item, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.pickerItemContainer}>
+        {!item.profilePic && (
+          <View style={[styles.iconContainer, styles.greyIcon]}>
+            <MaterialCommunityIcons
+              name="account"
+              size={30}
+              color={colors.primary}
+            />
+          </View>
+        )}
+        <View style={styles.textContainer}>
+          <AppText>{`${item.firstName} ${item.lastName}`}</AppText>
+          <AppText
+            style={styles.subText}
+          >{`${item.className} - ${item.section}`}</AppText>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -60,6 +93,13 @@ const styles = StyleSheet.create({
   dropDownIcon: {
     alignSelf: "center",
   },
+  greyIcon: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    height: 50,
+    justifyContent: "center",
+    width: 50,
+  },
   iconContainer: {
     alignItems: "center",
     backgroundColor: colors.white,
@@ -68,9 +108,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     width: 60,
   },
+  pickerItemContainer: {
+    flexDirection: "row",
+    padding: 10,
+  },
   subText: {
     fontSize: 14,
     color: colors.medium,
+    marginTop: 1,
   },
   textContainer: {
     alignSelf: "center",
