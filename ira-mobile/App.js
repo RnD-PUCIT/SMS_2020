56ffedc9f6f8663ba7b1b6326f9821b2dc8ebb54
Sohaib Salman <sohaibsalman10@gmail.com>
2021-04-29 17:10:54 +0500

    Add dashboard links in ParentDashboard.js

--- ira-mobile/App.js
+++ ira-mobile/App.js
@@ -1,60 +1,6 @@
-import React, { useState } from "react";
-
-import Screen from "./app/components/Screen";
-import StudentPicker from "./app/components/StudentPicker";
-
-const students = [
-  {
-    id: 1,
-    firstName: "Sohaib",
-    lastName: "Salman",
-    className: "BS IT",
-    section: "F17 Afternoon",
-    profilePic: "",
-  },
-  {
-    id: 2,
-    firstName: "Arslan",
-    lastName: "Yousaf",
-    className: "BS IT",
-    section: "F17 Afternoon",
-    profilePic: "",
-  },
-  {
-    id: 3,
-    firstName: "Daniyal",
-    lastName: "Ahmed",
-    className: "BS IT",
-    section: "F17 Afternoon",
-    profilePic: "",
-  },
-  {
-    id: 4,
-    firstName: "Tehreem",
-    lastName: "Akhter",
-    className: "BS IT",
-    section: "F17 Afternoon",
-    profilePic: "",
-  },
-  {
-    id: 5,
-    firstName: "Zainab",
-    lastName: "Zulfiqar",
-    className: "BS IT",
-    section: "F17 Afternoon",
-    profilePic: "",
-  },
-];
+import React from "react";
+import ParentDashboard from "./app/screens/ParentDashboard";
 
 export default function App() {
-  const [selectedStudent, setSelectedStudent] = useState(null);
-  return (
-    <Screen>
-      <StudentPicker
-        items={students}
-        onSelectItem={(item) => setSelectedStudent(item)}
-        selectedStudent={selectedStudent ? selectedStudent : students[0]}
-      />
-    </Screen>
-  );
+  return <ParentDashboard />;
 }
--- ira-mobile/app/components/StudentPicker.js
+++ ira-mobile/app/components/StudentPicker.js
@@ -118,6 +118,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     flexDirection: "row",
     padding: 10,
+    marginVertical: 10,
   },
   closeButton: {
     alignItems: "flex-end",
