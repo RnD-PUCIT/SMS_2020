607e0cafdf8197a30723044cfb05f57b66c55672
Zainab Zulfiqar <shahzainab952@gmail.com>
2021-05-03 01:45:26 +0500

    Notifications hub created

---
+++ IRAAPI/IRAAPI/SignalRHub/IConnectionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IRAAPI.SignalRHub
+{
+    public interface IConnectionManager
+    {
+        void AddConnection(string userId, string connectionId);
+        void RemoveConnection(string connectionId);
+        HashSet<string> GetConnections(string userId);
+    }
+}
---
+++ IRAAPI/IRAAPI/SignalRHub/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IRAAPI.SignalRHub
+{
+    public class Notification
+    {
+        public int userId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool isAlert { get; set; }
+    }
+}
