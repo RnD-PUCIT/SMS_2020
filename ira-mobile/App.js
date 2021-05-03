4cfbbcd1968c71651dec406f616475ae26cca160
Sohaib Salman <sohaibsalman10@gmail.com>
2021-05-03 22:18:23 +0500

    Configure announcement screen with navigation

--- ira-mobile/App.js
+++ ira-mobile/App.js
@@ -3,13 +3,11 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import DrawerNavigator from "./app/navigation/DrawerNavigator";
 import appTheme from "./app/navigation/navigationTheme";
-import AnnoucementsScreen from "./app/screens/AnnoucementsScreen";
 
 export default function App() {
   return (
-    // <NavigationContainer theme={appTheme}>
-    //   <DrawerNavigator />
-    // </NavigationContainer>
-    <AnnoucementsScreen />
+    <NavigationContainer theme={appTheme}>
+      <DrawerNavigator />
+    </NavigationContainer>
   );
 }
