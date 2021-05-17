bf1649d0be617522f399ea8503ada69d8d30fd4c
Sohaib Salman <sohaibsalman10@gmail.com>
2021-05-17 19:59:12 +0500

    Integrate search contact API with UI

--- intelligent_remote_academia-frontend/src/components/pages/chat/ChatNew.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/chat/ChatNew.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
+import { IconButton, makeStyles, TextField } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+import http from "../../../services/httpService";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function ChatNew({ open, onClose }) {
+  const classes = useStyles();
+
+  const [searchValue, setSearchValue] = useState("");
+  const [contact, setContact] = useState(null);
+
+  const handleSearch = async () => {
+    const url = `/chat?text=${searchValue}`;
+
+    try {
+      const { data } = await http.get(url);
+
+      console.log(data);
+
+      setContact(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Dialog
@@ -19,13 +41,22 @@ export default function ChatNew({ open, onClose }) {
         TransitionComponent={Transition}
         keepMounted
         onClose={onClose}
+        fullWidth
       >
         <DialogTitle>Select new contact</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
+          <div className={classes.searchContainer}>
+            <TextField
+              variant="outlined"
+              label="Search username"
+              fullWidth
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <IconButton color="primary" onClick={handleSearch}>
+              <SearchIcon />
+            </IconButton>
+          </div>
         </DialogContent>
         <DialogActions>
           <Button onClick={onClose} color="primary">
@@ -39,3 +70,9 @@ export default function ChatNew({ open, onClose }) {
     </div>
   );
 }
+
+const useStyles = makeStyles({
+  searchContainer: {
+    display: "flex",
+  },
+});
