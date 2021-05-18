6cf1fe30ccbd05f5c3132ecb5415ae70d5ade9f4
Sohaib Salman <sohaibsalman10@gmail.com>
2021-05-18 19:32:13 +0500

    Show chat date in chat list

--- intelligent_remote_academia-frontend/src/components/pages/chat/ChatList.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/chat/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Avatar,
   Divider,
@@ -19,14 +19,38 @@ import colors from "../../../colors";
 const ChatList = ({ onNewChat, selectedChatId, onChatChange, userId }) => {
   const classes = useStyles();
 
-  const [selectedChat, setSelectedChat] = useState(null);
-
   const chatsRef = firestore.collection("chats");
   const query = chatsRef
     .orderBy("timestamp", "desc")
     .where("users", "array-contains", userId);
 
   const [chats] = useCollectionData(query);
+
+  const setChatTime = (timestamp) => {
+    const current = new Date();
+    if (
+      current.getMonth() === timestamp.getMonth() &&
+      current.getDate() === timestamp.getDate() &&
+      current.getFullYear() === timestamp.getFullYear()
+    ) {
+      let hours = timestamp.getHours();
+      const AmOrPm = hours >= 12 ? "PM" : "AM";
+      hours = hours % 12 || 12;
+      let minutes = timestamp.getMinutes();
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      const finalTime = hours + ":" + minutes + " " + AmOrPm;
+      return finalTime;
+    } else {
+      let date = timestamp.getDate();
+      let month = timestamp.getMonth();
+      let year = timestamp.getFullYear();
+
+      date = date < 10 ? "0" + date : date;
+      month = month < 10 ? "0" + month : month;
+
+      return `${date}/${month}/${year}`;
+    }
+  };
 
   if (!chats) return null;
 
@@ -42,61 +66,69 @@ const ChatList = ({ onNewChat, selectedChatId, onChatChange, userId }) => {
         <Divider />
         <div className={classes.chatList + " " + "chatList"}>
           <List className={classes.root}>
-            {chats.map((item, index) => {
-              const user = item.userDetails.filter((u) => u.id !== userId)[0];
+            {chats &&
+              chats.map((item, index) => {
+                const user = item.userDetails.filter((u) => u.id !== userId)[0];
 
-              return (
-                <React.Fragment key={index}>
-                  <ListItem
-                    alignItems="flex-start"
-                    button
-                    style={{ alignItems: "center" }}
-                    selected={selectedChatId === item.chatId}
-                    onClick={() => {
-                      onChatChange(item.chatId);
-                    }}
-                  >
-                    <ListItemAvatar style={{ margin: 0 }}>
-                      <Avatar
-                        alt={user.name}
-                        src=""
-                        className={classes.image}
+                return (
+                  <React.Fragment key={index}>
+                    <ListItem
+                      alignItems="flex-start"
+                      button
+                      style={{ alignItems: "center" }}
+                      selected={selectedChatId === item.chatId}
+                      onClick={() => {
+                        onChatChange(item.chatId);
+                      }}
+                    >
+                      <ListItemAvatar style={{ margin: 0 }}>
+                        <Avatar
+                          alt={user.name}
+                          src=""
+                          className={classes.image}
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        style={{ margin: 0 }}
+                        primary={
+                          <div className={classes.mainContainer}>
+                            <Typography className={classes.userName}>
+                              {user.name}
+                            </Typography>
+                            <Typography
+                              color="textSecondary"
+                              className={classes.chatDate}
+                            >
+                              {item.timestamp &&
+                                setChatTime(item.timestamp.toDate())}
+                            </Typography>
+                          </div>
+                        }
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              component="span"
+                              variant="body2"
+                              className={classes.messageOutline}
+                              color="textSecondary"
+                              noWrap
+                            >
+                              {item.messageOutline
+                                ? item.messageOutline
+                                : "Say hi..."}
+                            </Typography>
+                          </React.Fragment>
+                        }
                       />
-                    </ListItemAvatar>
-                    <ListItemText
-                      style={{ margin: 0 }}
-                      primary={
-                        <React.Fragment>
-                          <Typography className={classes.userName}>
-                            {user.name}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            className={classes.messageOutline}
-                            color="textSecondary"
-                            noWrap
-                          >
-                            {item.messageOutline
-                              ? item.messageOutline
-                              : "Say hi..."}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                    />
-                  </ListItem>
-                  {index === chats.length - 1 ? (
-                    <React.Fragment />
-                  ) : (
-                    <Divider style={{ margin: "0 15px" }} />
-                  )}
-                </React.Fragment>
-              );
-            })}
+                    </ListItem>
+                    {index === chats.length - 1 ? (
+                      <React.Fragment />
+                    ) : (
+                      <Divider style={{ margin: "0 15px" }} />
+                    )}
+                  </React.Fragment>
+                );
+              })}
           </List>
         </div>
         <Fab color="primary" className={classes.addButton} onClick={onNewChat}>
@@ -118,7 +150,6 @@ const useStyles = makeStyles({
     padding: 10,
   },
   chatList: {
-    margin: "10px 0",
     height: "calc(100vh - 185px - 76px)",
     overflow: "auto",
   },
@@ -143,12 +174,19 @@ const useStyles = makeStyles({
     color: colors.primary,
     fontSize: 16,
     fontWeight: "500",
+    flex: "1",
   },
   searchField: {
     width: "100%",
     padding: "10px 15px",
     border: `1px solid ${colors.gray}`,
     borderRadius: 50,
+  },
+  mainContainer: {
+    display: "flex",
+  },
+  chatDate: {
+    fontSize: 14,
   },
 });
 
