500028b1320135c66abac4b81c1fe208c6eb123f
Sohaib Salman <sohaibsalman10@gmail.com>
2021-05-19 23:26:03 +0500

    Config jitsi settings

--- intelligent_remote_academia-frontend/src/components/pages/meeting/Meeting.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/meeting/Meeting.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { useJitsi } from "react-jutsu";
 
-const Meeting = () => {
+const Meeting = ({
+  roomName = "IRA JITSI MEET TEST",
+  displayName,
+  subject,
+}) => {
   const jitsiConfig = {
-    roomName: "IRA JITSI MEET TEST",
-    displayName: "Naruto Uzumaki",
+    roomName,
+    displayName,
+    subject,
     width: "100%",
-    subject: "fan",
     parentNode: "jitsi-container",
+    configOverwrite: {
+      startWithAudioMuted: true,
+      startWithVideoMuted: true,
+      liveStreamingEnabled: false,
+      fileRecordingsEnabled: false,
+      enableClosePage: false,
+      prejoinPageEnabled: false,
+    },
+    interfaceConfig: {
+      SHOW_WATERMARK_FOR_GUESTS: false,
+    },
   };
 
   const { loading, error, jitsi } = useJitsi(jitsiConfig);
 
+  console.log(jitsi);
   return (
     <React.Fragment>
       <div>
