ae2d56233fe244751cd28dd367db5ec99af39865
Sohaib Salman <sohaibsalman10@gmail.com>
2021-05-19 17:41:46 +0500

    Add dialog for creating PTM meeting

--- intelligent_remote_academia-frontend/src/components/pages/meeting/CreateMeeting.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/meeting/CreateMeeting.jsx
@@ -1,36 +1,47 @@
+import React, { useState } from "react";
 import { Grid, makeStyles, Paper, Typography } from "@material-ui/core";
-import React from "react";
-import colors from "../../../colors";
 
+import colors from "../../../colors";
 import FeatherIcon from "../../common/icons/FeatherIcon";
+import CreatePTM from "./CreatePTM";
 
 const CreateMeeting = () => {
   const classes = useStyles();
 
+  const [openCreatePTM, setOpenCreatePTM] = useState(false);
+
   return (
     <React.Fragment>
+      <Typography variant="h6" className={classes.heading}>
+        Create Meeting:
+      </Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Paper className="shadow">
-            <div className={classes.meetingTypeContainer}>
-              <FeatherIcon iconName="FiUser" size={50} color={colors.light} />
-              <Typography className={classes.meetingName}>
-                Single Time Meeting
-              </Typography>
-            </div>
-          </Paper>
-        </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className="shadow">
             <div className={classes.meetingTypeContainer}>
               <FeatherIcon iconName="FiUsers" size={50} color={colors.light} />
               <Typography className={classes.meetingName}>
-                Multi Time Meeting
+                Remote Class
+              </Typography>
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className="shadow">
+            <div
+              className={classes.meetingTypeContainer}
+              onClick={() => setOpenCreatePTM(true)}
+            >
+              <FeatherIcon iconName="FiUser" size={50} color={colors.light} />
+              <Typography className={classes.meetingName}>
+                Remote PTM
               </Typography>
             </div>
           </Paper>
         </Grid>
       </Grid>
+
+      <CreatePTM open={openCreatePTM} onClose={() => setOpenCreatePTM(false)} />
     </React.Fragment>
   );
 };
@@ -38,6 +49,9 @@ const CreateMeeting = () => {
 export default CreateMeeting;
 
 const useStyles = makeStyles({
+  heading: {
+    marginBottom: 10,
+  },
   meetingTypeContainer: {
     alignItems: "center",
     backgroundColor: colors.secondary,
