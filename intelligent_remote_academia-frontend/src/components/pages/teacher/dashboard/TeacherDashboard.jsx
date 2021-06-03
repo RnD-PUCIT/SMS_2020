816194e0e732d14ee4933c3c349acdaff7f12a32
Sohaib Salman <sohaibsalman10@gmail.com>
2021-06-03 20:41:46 +0500

    Create course list component for teacher dashboard

--- intelligent_remote_academia-frontend/src/components/pages/teacher/dashboard/TeacherDashboard.jsx
+++ intelligent_remote_academia-frontend/src/components/pages/teacher/dashboard/TeacherDashboard.jsx
@@ -1,21 +1,18 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import PageHeading from '../../../common/PageHeading';
+import TeacherCoursesList from './TeacherCoursesList';
 
 const TeacherDashboard = () => {
   return (
     <React.Fragment>
-      <h1>Teachers Dashboard</h1>
-      <ul>
-        <li>
-          <Link to='diary-form'>Diary</Link>
-        </li>
-        <li>
-          <Link to='mark-attendance'>Attendance</Link>
-        </li>
-        <li>
-          <Link to='classes'>Classes</Link>
-        </li>
-      </ul>
+      <PageHeading title="Teacher Dashboard" />
+      <Grid container spacing={3}>
+        <Grid item md={8}>
+          <TeacherCoursesList />
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
